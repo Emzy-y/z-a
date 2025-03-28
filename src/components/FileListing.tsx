34e9b6fb9140976e60645e7ae8d5a4096b0359c5
Emzy <85033866+Emzy-y@users.noreply.github.com>
2025-03-28 11:06:51 -0600

    Update FileListing.tsx

--- src/components/FileListing.tsx
+++ src/components/FileListing.tsx
@@ -74,13 +74,12 @@ export const ChildName: FC<{ name: string; folder?: boolean }> = ({ name, folder
   const extension = folder ? '' : getRawExtension(original)
   const prename = folder ? original : original.substring(0, original.length - extension.length)
   return (
-<div className="flex items-start gap-2">
-  <ChildIcon child={child} />
-  <span className="whitespace-normal break-all flex-1">
-    {prename}
-    {extension && <span className="text-gray-500">{extension}</span>}
-  </span>
-</div>
+    <div className="flex items-center">
+      <span className="whitespace-normal break-all">
+        {prename}
+        {extension && <span className="text-gray-500">{extension}</span>}
+      </span>
+    </div>
   )
 }
 export const ChildIcon: FC<{ child: OdFolderChildren }> = ({ child }) => {
